f7b9667d2df6bab3fa624fd98c0a0f683e831885
Pritamdas16 <pd4137928@gmail.com>
2025-12-08 17:11:23 +0530

    Prediction Pipeline

---
+++ app.py
@@ -0,0 +1,87 @@
+# from flask import Flask,request,render_template
+# import numpy as np
+# import pandas as pd
+
+# from sklearn.preprocessing import StandardScaler
+# from src.pipeline.predict_pipeline import CustomData,predictPipeline
+
+# application=Flask(__name__)
+
+# app=application
+
+# ## Route for a home page
+
+# @app.route('/')
+# def index():
+#     return render_template('index.html')
+
+# @app.route('/predictions',methods=['GET','POST'])
+# def predict_datapoint():
+#     if request.method=='GET':
+#         return render_template('home.html')
+#     else:
+#         data=CustomData(
+#             gender=request.from.get('gender'),
+#             race_ethnicity=request.from.get('ethnicity'),
+#             parental_level_of_education=request.from.get('parental_level_of_education'),
+#             lunch=request.from.get('lunch'),
+#             test_preparation_course=request.from.get('test_preparation_course'),
+#             reading_score=float(request.from.get('writing_score')),
+#             writing_score=float(request.from.get('reading_score'))
+
+#         )
+#         pred_df=data.get_data_as_data_frame()
+#         print(pred_df)
+
+#         predict_pipeline=PredictPipeline()
+#         results=predict_pipeline.predict(pred_df)
+#         return render_template('home.html',results=results[0])
+    
+
+from flask import Flask, request, render_template
+import numpy as np
+import pandas as pd
+
+from sklearn.preprocessing import StandardScaler
+from src.pipeline.predict_pipeline import CustomData, PredictPipeline
+
+application = Flask(__name__)
+app = application
+
+## Route for the home page
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/predictdata', methods=['GET', 'POST'])
+def predict_datapoint():
+    if request.method == 'GET':
+        return render_template('home.html')
+    else:
+        # FIX: request.form (NOT request.from)
+        data = CustomData(
+            gender=request.form.get('gender'),
+            race_ethnicity=request.form.get('ethnicity'),
+            parental_level_of_education=request.form.get('parental_level_of_education'),
+            lunch=request.form.get('lunch'),
+            test_preparation_course=request.form.get('test_preparation_course'),
+            reading_score=float(request.form.get('reading_score')),
+            writing_score=float(request.form.get('writing_score'))
+        )
+
+        # Convert to DataFrame
+        pred_df = data.get_data_as_data_frame()
+        print(pred_df)
+
+        # FIX: Class name should match import
+        predict_pipeline = PredictPipeline()
+
+        # Predict
+        results = predict_pipeline.predict(pred_df)
+
+        return render_template('home.html', results=results[0])
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
